fc39a4ace9ae52c7a67fcf628feafabdc3471c64
wellcomez <zhu.jialai@gmail.com>
2024-07-23 08:22:36 +0800

    add

--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -187,23 +187,7 @@ func MainUI() {
 	}
 }
 
-type CallStackEntry struct {
-	name string
-}
 
-type CallStack struct {
-	data []CallStackEntry
-}
-
-func NewCallStack() *CallStack {
-	ret := CallStack{}
-	return &ret
-}
-
-type LspCallInRecord struct {
-	name string
-	data []CallStack
-}
 type Search interface {
 	Findall(key string) []int
 }
