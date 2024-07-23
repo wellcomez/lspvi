6ee9dad1fbd683bdf89922100d552590aeeb53e3
wellcomez <zhu.jialai@gmail.com>
2024-07-23 11:01:30 +0800

    add

--- pkg/lsp/callin.go
+++ pkg/lsp/callin.go
@@ -20,9 +20,10 @@ func NewCallStackEntry(item lsp.CallHierarchyItem) *CallStackEntry {
 
 type CallInTask struct {
 	Name     string
-	Allstack []CallStack
+	Allstack []*CallStack
 	loc      lsp.Location
 	lsp      *lsp_base
+	set      map[lsp.Range]bool
 }
 type CallStack struct {
 	Items []*CallStackEntry
@@ -46,21 +47,61 @@ func NewCallInTask(loc lsp.Location, lsp *lsp_base) *CallInTask {
 	return task
 }
 
+type callchain struct {
+	parent *callchain
+	data   lsp.CallHierarchyItem
+	level  int
+}
+type added struct {
+	set []*callchain
+}
+
+func (task *CallInTask) addchild(parent *callchain, leaf *added) error {
+	child, err := task.lsp.CallHierarchyIncomingCalls(parent.data)
+	if err != nil || parent.level > 5 {
+		leaf.set = append(leaf.set, parent)
+		return err
+	}
+	add := false
+	for _, cc := range child {
+		if task.set[cc.From.Range] {
+			continue
+		}
+		top := &callchain{
+			data:   cc.From,
+			parent: parent,
+			level:  parent.level + 1,
+		}
+		task.addchild(top, leaf)
+		add = true
+	}
+	if !add {
+		leaf.set = append(leaf.set, parent)
+	}
+	return nil
+}
 func (task *CallInTask) run() error {
-	var ret []CallStack
 	c1, err := task.lsp.PrepareCallHierarchy(task.loc)
 	if err != nil {
 		return err
 	}
-	var stack CallStack
-	if len(c1) > 0 {
-		stack.Add(NewCallStackEntry(c1[0]))
-		c2, err := task.lsp.CallHierarchyIncomingCalls(c1[0])
-		if err == nil && len(c2) > 0 {
-			stack.Add(NewCallStackEntry(c2[0].From))
+	for _, item := range c1 {
+		var leaf added
+		top := &callchain{
+			data:   item,
+			parent: nil,
+			level:  0,
+		}
+		task.set[item.Range] = true
+		task.addchild(top, &leaf)
+		for _, v := range leaf.set {
+			stacks := &CallStack{}
+			for v != nil {
+				stacks.Add(NewCallStackEntry(v.data))
+				v = v.parent
+			}
+			task.Allstack = append(task.Allstack, stacks)
 		}
 	}
-	ret = append(ret, stack)
-	//sym.Handle.OnCallInViewChanged(ret)
 	return nil
 }
