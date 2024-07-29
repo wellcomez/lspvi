36c75b070f55161cd20b523bc73b59b77494c863
wellcomez <zhu.jialai@gmail.com>
2024-07-30 01:45:35 +0800

    user fzf

--- pkg/ui/pickerfile.go
+++ pkg/ui/pickerfile.go
@@ -1,18 +1,19 @@
 package mainui
 
 import (
+	"github.com/reinhrst/fzf-lib"
 	"io/ioutil"
 	"log"
 	"path/filepath"
 	"strings"
-	"sync"
 )
 
 type DirWalk struct {
-	query string
-	cur   *querytask
-	root  string
-	cb    func(t querytask)
+	query    string
+	cur      *querytask
+	root     string
+	cb       func(t querytask)
+	hayStack []string
 }
 type querytask struct {
 	// filename string
@@ -26,10 +27,11 @@ type file_picker_item struct {
 }
 
 func NewDirWalk(root string, cb func(t querytask)) *DirWalk {
-	return &DirWalk{root: root, cb: cb}
-}
 
-func (wk *DirWalk) UpdateQuery(query string) {
+	var hayStack = walk(root)
+	return &DirWalk{root: root, cb: cb, hayStack: hayStack}
+}
+func (wk *DirWalk) UpdateQueryOld(query string) {
 	cur := wk.cur
 	r := cur == nil || !strings.Contains(query, cur.query)
 	if cur != nil && cur.done {
@@ -48,69 +50,58 @@ func (wk *DirWalk) UpdateQuery(query string) {
 	}
 }
 
-func asyncWalk(wk *querytask, root string, wg *sync.WaitGroup, fileChan chan string, dirChan chan string) {
-	// 发送文件路径到通道
-	defer wg.Done()
-	newFunction1(root, wk, wg, fileChan, dirChan)
+func (wk *DirWalk) UpdateQuery(query string) {
+	wk.query = query
+	wk.cur = &querytask{
+		query: query,
+		ret:   []file_picker_item{},
+	}
+	go wk.Run()
 }
 
-func newFunction1(root string, wk *querytask, wg *sync.WaitGroup, fileChan chan string, dirChan chan string) {
+func (wk *DirWalk) asyncWalk(task *querytask, root string) {
+	var options = fzf.DefaultOptions()
+	// update any options here
+	// var hayStack = walk(root)
+	var myFzf = fzf.New(wk.hayStack, options)
+	var result fzf.SearchResult
+	myFzf.Search(task.query)
+	result = <-myFzf.GetResultChannel()
+	for _, v := range result.Matches {
+		task.ret = append(task.ret, file_picker_item{
+			name: strings.ReplaceAll(v.Key, root, ""),
+			path: v.Key,
+		})
+	}
+	var t querytask
+	var check = NewGitIgnore(root)
+	for _, v := range wk.cur.ret {
+		if !check.Ignore(v.path) {
+			t.ret = append(t.ret, v)
+		}
+	}
+	wk.cb(t)
+}
+func walk(root string) []string {
+	ret := []string{}
 	files, err := ioutil.ReadDir(root)
 	if err == nil {
 		for _, v := range files {
 			path := filepath.Join(root, v.Name())
 			if v.IsDir() {
-				newFunction1(path, wk, wg, fileChan, dirChan)
+				ret = append(ret, walk(path)...)
 			} else {
-				if strings.Contains(strings.ToLower(path), wk.query) {
-					fileChan <- path
-				}
-
+				ret = append(ret, path)
 			}
 
 		}
 	}
+	return ret
 }
 
 func (wk *DirWalk) Run() {
 	root := wk.root
-	walk := wk.cur
-	findfile(root, walk)
-	var ret querytask
-	var check =NewGitIgnore(root)
-	for _, v := range wk.cur.ret {
-		if !check.Ignore(v.path) {
-			ret.ret = append(ret.ret, v)
-		}
-	}
-	wk.cb(ret)
+	wk.asyncWalk(wk.cur, root)
+
 	log.Printf("Run")
-}
-func findfile(root string, task *querytask) {
-	// task
-	var wg sync.WaitGroup
-	fileChan := make(chan string)
-	dirChan := make(chan string)
-
-	// 启动异步遍历
-	wg.Add(1)
-	go asyncWalk(task, root, &wg, fileChan, dirChan)
-
-	// 处理结果
-	go func() {
-		wg.Wait()
-		close(fileChan)
-		close(dirChan)
-	}()
-
-	// 从通道中读取并处理结果
-	for file := range fileChan {
-		log.Println("File:", file)
-		task.ret = append(task.ret, file_picker_item{path: file, name: strings.Replace(file, root, "", -1)})
-	}
-
-	// for dir := range dirChan {
-	// 	log.Println("Directory:", dir)
-	// }
-	task.done = true
 }
