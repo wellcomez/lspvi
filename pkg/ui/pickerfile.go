194e9cf5272a2624f6eef8eee38ea3c19f65d5ac
wellcomez <zhu.jialai@gmail.com>
2024-07-29 14:35:06 +0800

    add

--- pkg/ui/pickerfile.go
+++ pkg/ui/pickerfile.go
@@ -2,71 +2,90 @@ package mainui
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"io/ioutil"
+	// "log"
+	// "os"
 	"path/filepath"
 	"strings"
 	"sync"
 )
 
 type DirWalk struct {
-	wg            sync.WaitGroup
-	resultChannel chan string
-	filename      string
-	query         string
-	ret           []string
+	query string
+	ret   []string
+	cur   *querytask
+	root  string
+}
+type querytask struct {
+	// filename string
+	query string
+	ret   []string
+	done  bool
 }
 
 func NewDirWalk(root string) *DirWalk {
-	return &DirWalk{
-		resultChannel: make(chan string),
-		filename:      root,
-	}
+	return &DirWalk{root: root}
 }
 
 func (wk *DirWalk) UpdateQuery(query string) {
-	var run = len(wk.query) == 0
+	cur := wk.cur
+	r := cur == nil || !strings.Contains(query, cur.query)
+	if cur != nil && cur.done {
+		r = true
+		cur.ret = []string{}
+	}
 	wk.query = query
-	if run {
-		go Run(wk)
+	if r {
+		wk.cur = &querytask{
+			query: query,
+		}
+	}
+	if r {
+		go wk.Run()
 	}
 }
 
-func asyncWalk(wk *DirWalk, root string, wg *sync.WaitGroup, fileChan chan string, dirChan chan string) {
-	defer wg.Done()
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Printf("Error walking path %s: %v\n", path, err)
-			return err
-		}
-		if info.IsDir() {
-			asyncWalk(wk, path, wg, fileChan, dirChan)
-		} else {
-			if strings.Contains(strings.ToLower(path), wk.query) {
-				fileChan <- path // 发送文件路径到通道
+func asyncWalk(wk *querytask, root string, wg *sync.WaitGroup, fileChan chan string, dirChan chan string) {
+	// 发送文件路径到通道
+	newFunction1(root, wk, wg, fileChan, dirChan)
+}
+
+func newFunction1(root string, wk *querytask, wg *sync.WaitGroup, fileChan chan string, dirChan chan string) {
+	files, err := ioutil.ReadDir(root)
+	if err == nil {
+		for _, v := range files {
+			path := filepath.Join(root, v.Name())
+			if v.IsDir() {
+				newFunction1(path, wk, wg, fileChan, dirChan)
+			} else {
+				if strings.Contains(strings.ToLower(path), wk.query) {
+					fileChan <- path
+				}
+
 			}
-		}
-		return nil
-	})
 
-	if err != nil {
-		fmt.Printf("Error walking the path %s: %v\n", root, err)
+		}
 	}
 }
 
-func Run(walk *DirWalk) {
-	go findfile(walk)
+func (wk *DirWalk) Run() {
+	root := wk.root
+	walk := wk.cur
+	findfile(root, walk)
 	println("Run")
 }
-func findfile(walk *DirWalk) {
-
+func findfile(root string, walk *querytask) {
+	// task
+	task := &querytask{
+		query: walk.query,
+	}
 	var wg sync.WaitGroup
 	fileChan := make(chan string)
 	dirChan := make(chan string)
 
 	// 启动异步遍历
 	wg.Add(1)
-	go asyncWalk(walk, walk.filename, &wg, fileChan, dirChan)
+	go asyncWalk(task, root, &wg, fileChan, dirChan)
 
 	// 处理结果
 	go func() {
@@ -78,10 +97,11 @@ func findfile(walk *DirWalk) {
 	// 从通道中读取并处理结果
 	for file := range fileChan {
 		fmt.Println("File:", file)
-		walk.ret = append(walk.ret, file)
+		task.ret = append(task.ret, file)
 	}
 
 	for dir := range dirChan {
 		fmt.Println("Directory:", dir)
 	}
+	walk.done = true
 }
