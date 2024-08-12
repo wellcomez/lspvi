f7be8944381ebcf01e21e6d87e366192199d9b1e
wellcomez <zhu.jialai@gmail.com>
2024-08-12 21:02:55 +0800

    add build

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
-git submodule init  && git submodule update --recursive
-pushd pkg/lspr
-bash build.sh -w
-popd
-go build
+git submodule init && git submodule update --recursive
+build_os() {
+  pushd pkg/lspr
+  bash build.sh -w
+  popd
+  go build -o lspvi-$GOOS
+}
+# export GOARCH=amd64
+export CGO_ENABLED=0 
+export GOOS=darwin
+build_os
+export GOOS=linux
+build_os
+
