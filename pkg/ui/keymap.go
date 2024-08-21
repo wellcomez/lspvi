ff46285a0fc0fcab671b54a5d8ab70de6031556d
wellcomez <zhu.jialai@gmail.com>
2024-08-21 15:40:28 +0800

    add

--- pkg/ui/keymap.go
+++ pkg/ui/keymap.go
@@ -55,21 +55,21 @@ func get_cmd_actor(m *mainui, id command_id) cmdactor {
 	case cmd_quit:
 		return cmdactor{"Quit", m.quit}
 	case open_picker_qfh:
-		return cmdactor{"Quickfix history", m.open_qfh_query}
+		return cmdactor{"quickfix history", m.open_qfh_query}
 	case open_picker_wkq:
-		return cmdactor{"Query workspace symbol", m.open_wks_query}
+		return cmdactor{"query workspace symbol", m.open_wks_query}
 	case open_picker_document_symbol:
-		return cmdactor{"Open symbol", m.open_document_symbol_picker}
+		return cmdactor{"open symbol", m.open_document_symbol_picker}
 	case open_picker_refs:
-		return cmdactor{"Reference", m.open_picker_refs}
+		return cmdactor{"reference", m.open_picker_refs}
 	case open_picker_livegrep:
-		return cmdactor{"Live grep", m.open_picker_livegrep}
+		return cmdactor{"live grep", m.open_picker_livegrep}
 	case open_picker_history:
-		return cmdactor{"History", m.open_picker_history}
+		return cmdactor{"history", m.open_picker_history}
 	case open_picker_grep_word:
-		return cmdactor{"Grep word", func() { m.codeview.action_grep_word() }}
+		return cmdactor{"grep word", func() { m.codeview.action_grep_word() }}
 	case open_picker_ctrlp:
-		return cmdactor{"Picker file", m.open_picker_ctrlp}
+		return cmdactor{"picker file", m.open_picker_ctrlp}
 	case goto_back:
 		{
 			return cmdactor{"back", func() {
@@ -244,7 +244,7 @@ func (m *mainui) key_map_space_menu() []cmditem {
 		get_cmd_actor(m, open_picker_grep_word).menu_key(split(key_picker_grep_word)),
 		get_cmd_actor(m, open_picker_ctrlp).menu_key(split(key_picker_ctrlp)),
 		get_cmd_actor(m, open_picker_help).menu_key(split(key_picker_help)),
-		get_cmd_actor(m, cmd_quit).menu_key(split("e")),
+		get_cmd_actor(m, cmd_quit).menu_key(split("Q")),
 	}
 }
 
