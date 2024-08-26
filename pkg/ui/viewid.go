39201c56e4025f1beed5af39a0f5d88fea7a3db4
wellcomez <zhu.jialai@gmail.com>
2024-08-26 18:53:18 +0800

    add

--- pkg/ui/viewid.go
+++ pkg/ui/viewid.go
@@ -118,6 +118,9 @@ func view_id_init(m *mainui) {
 					box.SetFocusFunc(func() {
 						m.editor_area_fouched()
 						change_after_focused(box, m)
+						if m.cmdline.Vim.vi.String() == "none" {
+							m.cmdline.Vim.EnterEscape()
+						}
 					})
 				}
 			case view_quickview, view_callin, view_uml:
