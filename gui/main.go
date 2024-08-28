6b1ea880b631cc61a1e55cd0b94ba39f5db790a1
wellcomez <zhu.jialai@gmail.com>
2024-08-28 13:36:06 +0800

    add

--- gui/main.go
+++ gui/main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"flag"
-
-	mainui "zen108.com/lspvi/pkg/ui"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	root := flag.String("root", "", "root-dir")
-	file := flag.String("file", "", "source file")
-	flag.Parse()
-	var arg = &mainui.Arguments{
-		Root: *root,
-		File: *file,
-	}
-	mainui.MainUI(arg)
+	// root := flag.String("root", "", "root-dir")
+	// file := flag.String("file", "", "source file")
+	// flag.Parse()
+	// var arg = &mainui.Arguments{
+	// 	Root: *root,
+	// 	File: *file,
+	// }
+	// mainui.MainUI(arg)
+
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+
 }
