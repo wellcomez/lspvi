545f5e8bf61df88b4a9d2f9e2fae111ab8e8762a
wellcomez <zhu.jialai@gmail.com>
2024-08-30 12:58:46 +0800

    add

---
+++ pkg/ui/boxresize.go
@@ -0,0 +1,11 @@
+package mainui
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+type ui_reszier struct {
+
+}
+func (resize*ui_reszier) handle_mouse_impl(action tview.MouseAction, event *tcell.EventMouse) (tview.MouseAction, *tcell.EventMouse) {
+}
