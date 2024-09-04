93142f91ec7c106fe7d41a5c83e3ff0fe08113be
wellcomez <zhu.jialai@gmail.com>
2024-09-04 11:49:30 +0800

    换到ws

--- xterm/xterm.go
+++ xterm/xterm.go
@@ -25,12 +25,34 @@ type wsize struct {
 	Width  uint16 `json:"width"`
 	Height uint16 `json:"height"`
 }
+type base_type interface{}
 type keycode struct {
 	Key string `json:"key"`
 }
+type wsbase struct {
+	Call string `json:"call"`
+}
+type init_call struct {
+	Call string `json:"call"`
+}
+type keydata struct {
+	Call string `json:"call"`
+	Data string `json:"data"`
+}
+type resize struct {
+	Call string `json:"call"`
+	Cols uint16 `json:"cols"`
+	Rows uint16 `json:"rows"`
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	conn, err := upgrader.Upgrade(w, r, nil)
+	sss.imp.ws = conn
 	if err != nil {
 		// handle error
 		return
@@ -40,13 +62,43 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	for {
 		messageType, message, err := conn.ReadMessage()
 		if err != nil {
-			// handle error
-			break
+			log.Println(messageType, message, err)
+			continue
 		}
-		log.Printf("Received: %s", message)
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			break
+		var w init_call
+		err = json.Unmarshal(message, &w)
+		if err == nil {
+			switch w.Call {
+			case "init":
+				{
+					sss.imp._send(sss.imp.unsend)
+				}
+			case "resize":
+				{
+					var res resize
+					err = json.Unmarshal(message, &res)
+
+					if err == nil {
+						if res.Rows != ptystdio.Rows || res.Cols != ptystdio.Cols {
+							ptystdio.UpdateSize(res.Rows, res.Cols)
+						}
+						// ptystdio.File.Write([]byte(key.Data))
+						continue
+					}
+				}
+			case "key":
+				{
+					var key keydata
+					err = json.Unmarshal(message, &key)
+					if err == nil {
+						ptystdio.File.Write([]byte(key.Data))
+						continue
+					}
+				}
+			default:
+				fmt.Println("unknown call", w.Call)
+			}
+			continue
 		}
 	}
 }
@@ -74,8 +126,6 @@ func NewRouter(root string) *mux.Router {
 			}
 		}
 	})
-	r.HandleFunc("/mouse", func(w http.ResponseWriter, r *http.Request) {
-	})
 	r.HandleFunc("/term", func(w http.ResponseWriter, r *http.Request) {
 		var k wsize
 		buf, err := io.ReadAll(r.Body)
@@ -95,7 +145,7 @@ func NewRouter(root string) *mux.Router {
 		}
 		oldlen := len(sss.imp.pty)
 		different := false
-		for i, _ := range sss.imp.output {
+		for i := range sss.imp.output {
 			if i < oldlen {
 				if sss.imp.output[i] != sss.imp.pty[i] {
 					fmt.Println(time.Now(), "output changed", i, sss.imp.output[i], sss.imp.pty[i])
@@ -128,34 +178,61 @@ func NewRouter(root string) *mux.Router {
 	return r
 }
 
-func StartServer(root string, port int, cb func(port int)) {
+func StartServer(root string, port int) {
 	r := NewRouter(root)
 	for i := port; i < 30000; i++ {
-		if cb != nil {
-			cb(i)
-		}
-		log.Printf("Server listening on http://localhost:%d\n", i)
+		// fmt.Printf("Server listening on http://localhost:%d\n", i)
 		if err := http.ListenAndServe(fmt.Sprintf(":%d", i), r); err != nil {
-			log.Println(i, "Inused")
+			fmt.Println(i, "Inused")
 		}
 	}
 }
 
 type ptyout_impl struct {
 	output string
+	prev   string
 	pty    string
+	ws     *websocket.Conn
+	unsend string
 }
+
+func (imp *ptyout_impl) send(s string) {
+	if imp.prev != s {
+		imp._send(s)
+	}
+}
+
+func (imp *ptyout_impl) _send(s string) bool {
+	fmt.Println("_send", len(s))
+	data := map[string]string{
+		"output": s,
+		"type":   "term",
+	}
+	buf, err := json.Marshal(data)
+	if imp.ws == nil {
+		imp.unsend += s
+		return true
+	}
+	if err == nil {
+		imp.ws.WriteMessage(websocket.TextMessage, buf)
+		imp.prev = s
+	}
+	return false
+}
+
 type ptyout struct {
 	imp *ptyout_impl
 }
 
 // Write implements pty.ptyio.
 func (p ptyout) Write(s []byte) (n int, err error) {
-	p.imp.output += string(s)
-	if need {
-		wg.Done()
-		need = false
-	}
+	p.imp.send(string(s))
+	// fmt.Println("xxx",len(s),string(s))
+	// p.imp.output += string(s)
+	// if need {
+	// 	wg.Done()
+	// 	need = false
+	// }
 	return len(s), nil
 }
 
@@ -167,5 +244,5 @@ func main() {
 		io.Copy(sss, ptystdio.File)
 		os.Exit(-1)
 	}()
-	StartServer(filepath.Dir(os.Args[0]), 13000, nil)
+	StartServer(filepath.Dir(os.Args[0]), 13000)
 }
