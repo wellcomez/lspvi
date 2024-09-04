c46e83dc830cf98916bdb071d6d8d4c816b08397
wellcomez <zhu.jialai@gmail.com>
2024-09-04 14:23:41 +0800

    include index.hml

--- pkg/ui/xterm.go
+++ pkg/ui/xterm.go
@@ -1,6 +1,7 @@
 package mainui
 
 import (
+	"embed"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -9,7 +10,7 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
-	"time"
+	// "time"
 
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
@@ -149,66 +150,71 @@ func NewRouter(root string) *mux.Router {
 		w.Write(buf)
 	}).Methods("GET")
 	r.HandleFunc("/ws", serveWs)
-	r.HandleFunc("/key", func(w http.ResponseWriter, r *http.Request) {
-		var k keycode
-		buf, err := io.ReadAll(r.Body)
-		if err == nil {
-			err = json.Unmarshal(buf, &k)
-			if err == nil {
-				if ptystdio != nil {
-					ptystdio.File.Write([]byte(k.Key))
-				}
-			}
-		}
-	})
-	r.HandleFunc("/term", func(w http.ResponseWriter, r *http.Request) {
-		var k wsize
-		buf, err := io.ReadAll(r.Body)
-		if err == nil {
-			if json.Unmarshal(buf, &k) == nil {
-				if k.Height != ptystdio.Rows || k.Width != ptystdio.Cols {
-					ptystdio.UpdateSize(k.Height, k.Width)
-				}
-			}
-		}
-		if len(sss.imp.output) == 0 {
-			wg.Wait()
-		}
+	// r.HandleFunc("/key", func(w http.ResponseWriter, r *http.Request) {
+	// 	var k keycode
+	// 	buf, err := io.ReadAll(r.Body)
+	// 	if err == nil {
+	// 		err = json.Unmarshal(buf, &k)
+	// 		if err == nil {
+	// 			if ptystdio != nil {
+	// 				ptystdio.File.Write([]byte(k.Key))
+	// 			}
+	// 		}
+	// 	}
+	// })
+	// r.HandleFunc("/term", func(w http.ResponseWriter, r *http.Request) {
+	// 	var k wsize
+	// 	buf, err := io.ReadAll(r.Body)
+	// 	if err == nil {
+	// 		if json.Unmarshal(buf, &k) == nil {
+	// 			if k.Height != ptystdio.Rows || k.Width != ptystdio.Cols {
+	// 				ptystdio.UpdateSize(k.Height, k.Width)
+	// 			}
+	// 		}
+	// 	}
+	// 	if len(sss.imp.output) == 0 {
+	// 		wg.Wait()
+	// 	}
 
-		if sss.imp == nil {
-			w.Write([]byte("xx"))
-		}
-		oldlen := len(sss.imp.pty)
-		different := false
-		for i := range sss.imp.output {
-			if i < oldlen {
-				if sss.imp.output[i] != sss.imp.pty[i] {
-					fmt.Println(time.Now(), "output changed", i, sss.imp.output[i], sss.imp.pty[i])
-					different = true
-				}
-			} else {
-				fmt.Println(time.Now(), "output longger", len(sss.imp.output), len(sss.imp.pty))
-				different = true
-			}
-		}
-		if !different {
-			w.Write([]byte("0000"))
-			return
-		}
-		sss.imp.pty = sss.imp.output
-		w.Write([]byte(sss.imp.pty))
-		wg.Add(1)
-		sss.imp.output = ""
-		need = true
-	})
+	// 	if sss.imp == nil {
+	// 		w.Write([]byte("xx"))
+	// 	}
+	// 	oldlen := len(sss.imp.pty)
+	// 	different := false
+	// 	for i := range sss.imp.output {
+	// 		if i < oldlen {
+	// 			if sss.imp.output[i] != sss.imp.pty[i] {
+	// 				fmt.Println(time.Now(), "output changed", i, sss.imp.output[i], sss.imp.pty[i])
+	// 				different = true
+	// 			}
+	// 		} else {
+	// 			fmt.Println(time.Now(), "output longger", len(sss.imp.output), len(sss.imp.pty))
+	// 			different = true
+	// 		}
+	// 	}
+	// 	if !different {
+	// 		w.Write([]byte("0000"))
+	// 		return
+	// 	}
+	// 	sss.imp.pty = sss.imp.output
+	// 	w.Write([]byte(sss.imp.pty))
+	// 	wg.Add(1)
+	// 	sss.imp.output = ""
+	// 	need = true
+	// })
 	// 处理根路径
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// if !need {
 		// 	wg.Add(1)
 		// 	need = true
 		// }
-		buf, _ := os.ReadFile("index.html")
-		w.Write(buf)
+		p := filepath.Join("html", "index.html")
+		buf, err := uiFS.Open(p)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+			return
+		}
+		io.Copy(w, buf)
 	}).Methods("GET")
 	return r
 }
@@ -217,8 +223,9 @@ func StartServer(root string, port int) {
 	r := NewRouter(root)
 	for i := port; i < 30000; i++ {
 		// fmt.Printf("Server listening on http://localhost:%d\n", i)
+		fmt.Println(i, "Check")
 		if err := http.ListenAndServe(fmt.Sprintf(":%d", i), r); err != nil {
-			fmt.Println(i, "Inused")
+			continue
 		}
 	}
 }
@@ -271,6 +278,9 @@ type ptyout struct {
 	imp *ptyout_impl
 }
 
+//go:embed  html
+var uiFS embed.FS
+
 // Write implements pty.ptyio.
 func (p ptyout) Write(s []byte) (n int, err error) {
 	p.imp.send(string(s))
@@ -285,7 +295,7 @@ func (p ptyout) Write(s []byte) (n int, err error) {
 
 // main
 func StartWebUI() {
-	argnew := []string{os.Args[0],"-tty"}
+	argnew := []string{os.Args[0], "-tty"}
 	args := os.Args[2:]
 	argnew = append(argnew, args...)
 	sss.imp.files.Files = []open_file{}
