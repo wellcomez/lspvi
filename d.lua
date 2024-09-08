c040cf393ac42b5103aad3feaceac5efbe69b353
zjl <zhujialai@icloud.com>
2024-09-08 22:00:03 +0800

    add

---
+++ d.lua
@@ -0,0 +1,116 @@
+-- Define highlight groups and output file path
+---@type
+local highlight_groups = {
+    "Comment",
+    --"Variable",
+    "Constant",
+    "String",
+    "Function",
+    "Keyword",
+    "Type",
+    "Character",
+    "Number",
+    "Boolean",
+    "Float",
+    "Identifier",
+    "Function",
+    "Statement",
+    "Conditional",
+    "Repeat",
+    "Label",
+    "Operator",
+    "Keyword",
+    "Exception",
+    "PreProc",
+    "Include",
+    "Define",
+    "Macro",
+    "PreCondit",
+    "Type",
+    "StorageClass",
+    "Structure",
+    "Typedef",
+    "Special",
+    "SpecialChar",
+    "Tag",
+    "Delimiter",
+    "SpecialComment",
+    "Debug",
+    "Underlined",
+    "Ignore",
+    "Error",
+    "Added",
+    "Changed",
+    "Removed",
+    "CursorLine",
+    "CursorColumn",
+    "Visual",
+    "StatusLine",
+    "Normal",
+    "DiagnosticError",
+    "DiagnosticWarn",
+    "DiagnosticInfo",
+    "DiagnosticHint",
+    --> 'TSKeyword', 'TSFunction','TSMethod',
+    "DiffAdd",
+    "DiffChange",
+    "DiffDelete",
+    "LspReferenceText",
+    "LspReferenceRead",
+    "LspReferenceWrite",
+    "@variable",
+    "@variable.member",
+    "@variable.parameter",
+    "@function",
+    "@function.method",
+    "@function.call",
+    "@keyword",
+    "@keyword.type",
+    "@keyword.Conditional",
+    "@keyword.function",
+    "@keyword.return",
+    "@keyword.modifier",
+    "@keyword.import",
+    "@module",
+    "@String",
+    "@type",
+    "@type.definition",
+    "@type.class",
+    "@property",
+    "@Operator",
+    "@Boolean",
+    "@Comment",
+    "@Constant",
+    "@Constant.builtin"
+}
+
+local output_file = "all_highlight_groups_info.txt" -- Path to the output file
+
+-- Function to get highlight settings and write to a file
+local function save_highlights_to_file(groups, file)
+    -- Open the file for writing
+    local f = io.open(file, "w")
+    if not f then
+        print("Error opening file!")
+        return
+    end
+
+    -- Write the highlight settings to the file
+    for _, group in ipairs(groups) do
+        local hl = vim.api.nvim_get_hl_by_name(group, true)
+        f:write("Highlight Group: " .. group .. "\n")
+        for k, v in pairs(hl) do
+            f:write(tostring(k) .. ": " .. tostring(v) .. "\n")
+        end
+        f:write("\n")
+    end
+
+    -- Close the file
+    f:close()
+
+    print("Highlight settings saved to " .. file)
+end
+
+-- Call the function
+save_highlights_to_file(highlight_groups, output_file)
+--TSVariable
