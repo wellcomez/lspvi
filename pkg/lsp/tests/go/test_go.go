8fe3809ad8518c4ce2b9a0968026ba0a6af4a3b1
wellcomez <zhu.jialai@gmail.com>
2024-10-09 17:48:11 +0800

    test: add interface implementation to struct1 in go test
    - Introduce an interface 'inf' with two methods: call1() and call2()
    - Implement the 'inf' interface using the existing struct1
    - This change enhances the test by incorporating interface usage, demonstrating
      the capability to implement interfaces in Go

--- pkg/lsp/tests/go/test_go.go
+++ pkg/lsp/tests/go/test_go.go
@@ -1,8 +1,12 @@
 package main
 
+type inf interface {
+	call1()
+	call2()
+}
 type struct1 struct {
 	a, b, c int
 }
 
-func (a struct1) call1()    {}
+func (a struct1) call1()  {}
 func (a *struct1) call2() {}
