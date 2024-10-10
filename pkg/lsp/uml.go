2fe2536d8adca7117fe8d335cfc1b829027076c7
wellcomez <zhu.jialai@gmail.com>
2024-10-10 10:36:51 +0800

    refactor

--- pkg/lsp/uml.go
+++ pkg/lsp/uml.go
@@ -43,8 +43,68 @@ func (entry CallStackEntry) uml_class_name() string {
 func (entry CallStackEntry) symboldefine_name() string {
 	return entry.Item.Name
 }
+func (call CallStack) newuml() (ret []string, title string) {
+	var caller *CallStackEntry = nil
+	ret = make([]string, 0)
+	for _, s := range call.Items {
+		rightPrefix := ""
+		s_is_class := !s.isFunction()
+		if s_is_class {
+
+			if len(s.uml_class_name()) > 0 {
+				rightPrefix = s.uml_class_name() + "::"
+			}
+		}
+		right := rightPrefix + s.symboldefine_name()
+		if strings.Index(right, "ProcessInternal") > 0 {
+			log.Println(right, s.symboldefine_name(), s.uml_class_name())
+		}
+		if len(ret) == 0 {
+			title = fmt.Sprintf("==%s==", right)
+		}
+		if caller != nil {
+			if s_is_class {
+				left := caller.symboldefine_name()
+				if len(caller.uml_class_name()) > 0 {
+					left = caller.uml_class_name()
+				}
+
+				ret = append(ret, fmt.Sprintf("%s -> %s", left, right))
+			} else {
+				left := caller.uml_class_name()
+				if caller.isFunction() || len(left) == 0 {
+					left = caller.symboldefine_name()
+				}
+				ret = append(ret, fmt.Sprintf("%s -> %s", left, right))
+			}
+		}
+		caller = s
+	}
+	return ret, title
+}
 
 func (call CallStack) Uml(markdown bool) string {
+	//go use function as method because treate package as object
+	ret, title := call.olduml()
+	markBegin := ""
+	if markdown {
+		markBegin = "```plantuml"
+	}
+	var black = "skinparam monochrome true"
+
+	sss := []string{markBegin, "@startuml", black, "autoactivate on", title}
+	sss = append(sss, ret...)
+	markEnd := ""
+	if markdown {
+
+		markEnd = "```\n\n\n"
+	}
+	sss = append(sss, "@enduml", markEnd)
+
+	return strings.Join(sss, "\n")
+}
+
+func (call CallStack) olduml() ([]string, string) {
 	ret := make([]string, 0)
 	var caller *CallStackEntry = nil
 
@@ -52,7 +112,7 @@ func (call CallStack) Uml(markdown bool) string {
 	for _, s := range call.Items {
 		rightPrefix := ""
 		if !s.isFunction() {
-			//go use function as method because treate package as object
+
 			if len(s.uml_class_name()) > 0 {
 				rightPrefix = s.uml_class_name() + "::"
 			}
@@ -87,22 +147,7 @@ func (call CallStack) Uml(markdown bool) string {
 		}
 		caller = s
 	}
-	markBegin := ""
-	if markdown {
-		markBegin = "```plantuml"
-	}
-	var black = "skinparam monochrome true"
-
-	sss := []string{markBegin, "@startuml", black, "autoactivate on", title}
-	sss = append(sss, ret...)
-	markEnd := ""
-	if markdown {
-
-		markEnd = "```\n\n\n"
-	}
-	sss = append(sss, "@enduml", markEnd)
-
-	return strings.Join(sss, "\n")
+	return ret, title
 }
 
 // 假设applyFix函数被定义为应用fix函数到stack中的每个元素
