db83b2fb5c3812a01814015b7bda0b680e4fa031
wellcomez <zhu.jialai@gmail.com>
2024-10-13 12:31:35 +0800

    feat(ui): enhance log view with dynamic colors and formatted headers
    - Add dynamic colors to log view text
    - Implement formatted headers for log entries
    - Improve log entry layout and readability

--- pkg/ui/lgoview.go
+++ pkg/ui/lgoview.go
@@ -18,6 +18,7 @@ func new_log_view(main *mainui) *logview {
 		view_link: &view_link{id: view_log, up: view_code, right: view_callin},
 		log:       tview.NewTextView(),
 	}
+	ret.log.SetDynamicColors(true)
 	return ret
 }
 func (log *logview) clean() {
@@ -27,5 +28,10 @@ func (log *logview) update_log_view(s string) {
 	log.lineno++
 	t := log.log.GetText(true)
 	customLayout := "2006-01-02 15:04:05.000"
-	log.log.SetText(t + fmt.Sprintf("%d: %v\n", log.lineno, time.Now().Format(customLayout)) + s + "\n")
+	h:=fmt.Sprintf("%d: %v", log.lineno, time.Now().Format(customLayout))
+	
+	h=fmt.Sprintf("[#8080ff]%s[#ffffff]",h)
+	x := t + h+"\n"
+	log.log.SetText(
+		x + s + "\n")
 }
