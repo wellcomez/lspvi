0ca87a22636685bb5e1055e3c8be5d06f6c9de80
wellcomez <zhu.jialai@gmail.com>
2024-10-13 21:49:04 +0800

    test(ui): add grep test for Go string handling
    Introduce a new test case in the UI package for experimenting with string manipulation functions, specifically testing the IndexAll function from the go-string package. This test reads a file and checks for occurrences of the substring "err", logging

---
+++ pkg/ui/grep_test.go
@@ -0,0 +1,18 @@
+package mainui
+
+import (
+	"os"
+	"testing"
+
+	str "github.com/boyter/go-string"
+)
+func TestGoString(t *testing.T) {
+	if _,err:=os.ReadFile("/home/z/dev/lsp/goui/pkg/ui/gogrepimpl.go");err==nil{
+		// sss:=string(d)
+		ret:=str.IndexAll("err123456\nerr","err",-1)	
+		for _, v := range ret {
+			t.Log(v)
+		}
+	}
+
+}
