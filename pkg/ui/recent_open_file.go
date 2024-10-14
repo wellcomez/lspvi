798d569b53296da2fdfc98f5c21b1a8fd7341c81
wellcomez <zhu.jialai@gmail.com>
2024-10-14 11:32:24 +0800

    refactor(ui): move recent_open_file struct and its methods to separate file

---
+++ pkg/ui/recent_open_file.go
@@ -0,0 +1,35 @@
+package mainui
+type recent_open_file struct {
+	list *customlist
+	*view_link
+	filelist []string
+	main     *mainui
+	Name     string
+}
+
+func (r *recent_open_file) add(filename string) {
+	go func() {
+		GlobalApp.QueueUpdateDraw(func() {
+			for _, v := range r.filelist {
+				if v == filename {
+					return
+				}
+			}
+			filepath := filename
+			r.filelist = append(r.filelist, filename)
+			filename = trim_project_filename(filename, global_prj_root)
+			r.list.AddItem(filename, "", func() {
+				r.main.OpenFileHistory(filepath, nil)
+			})
+		})
+	}()
+}
+func new_recent_openfile(m *mainui) *recent_open_file {
+	return &recent_open_file{
+		list:      new_customlist(false),
+		view_link: &view_link{id: view_recent_open_file},
+		filelist:  []string{},
+		main:      m,
+		Name:      "Opened Files",
+	}
+}
--- pkg/ui/ui.go
+++ pkg/ui/ui.go
@@ -63,40 +63,7 @@ type rootlayout struct {
 	spacemenu   *space_menu
 	// hide_cb     func()
 }
-type recent_open_file struct {
-	list *customlist
-	*view_link
-	filelist []string
-	main     *mainui
-	Name     string
-}
 
-func (r *recent_open_file) add(filename string) {
-	go func() {
-		GlobalApp.QueueUpdateDraw(func() {
-			for _, v := range r.filelist {
-				if v == filename {
-					return
-				}
-			}
-			filepath := filename
-			r.filelist = append(r.filelist, filename)
-			filename = trim_project_filename(filename, global_prj_root)
-			r.list.AddItem(filename, "", func() {
-				r.main.OpenFileHistory(filepath, nil)
-			})
-		})
-	}()
-}
-func new_recent_openfile(m *mainui) *recent_open_file {
-	return &recent_open_file{
-		list:      new_customlist(false),
-		view_link: &view_link{id: view_recent_open_file},
-		filelist:  []string{},
-		main:      m,
-		Name:      "Opened Files",
-	}
-}
 
 type MainService interface {
 	Close()
