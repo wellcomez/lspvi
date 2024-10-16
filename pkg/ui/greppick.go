7f95d37791eb5aa0ae4f23c36e70b5e9d7b5d995
wellcomez <zhu.jialai@gmail.com>
2024-10-16 11:52:22 +0800

    refactor(ui): split greppicker into separate file
    The greppicker functionality has been moved to a new file, greppick.go, to better organize the codebase. This change separates the Live grep picker and the Grep word picker, making the code easier to manage and understand.

---
+++ pkg/ui/greppick.go
@@ -0,0 +1,72 @@
+package mainui
+
+import (
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+func new_grep_picker(v *fzfmain, code CodeEditor) *greppicker {
+	grep := &greppicker{
+		livewgreppicker: new_live_grep_picker(v, code),
+	}
+	grep.not_live = true
+
+	return grep
+}
+// greppicker
+type greppicker struct {
+	*livewgreppicker
+}
+// close implements picker.
+func (g *greppicker) close() {
+	g.livewgreppicker.close()
+}
+
+
+// UpdateQuery implements picker.
+// Subtle: this method shadows the method (*livewgreppicker).UpdateQuery of greppicker.livewgreppicker.
+func (g *greppicker) UpdateQuery(query string) {
+	// g.query = query
+	if g.impl.fzf_on_result != nil {
+		g.impl.fzf_on_result.OnSearch(query, true)
+		g.grep_list_view.Key = query
+	}
+}
+
+// handle implements picker.
+// Subtle: this method shadows the method (*livewgreppicker).handle of greppicker.livewgreppicker.
+func (g *greppicker) handle() func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+	return func(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
+		focused := g.grep_list_view.HasFocus()
+		var key = event.Key()
+		if key == tcell.KeyEnter && !focused {
+			if g.impl.last != g.impl.query_option {
+				RunQuery(g)
+			} else {
+				g.grep_list_view.InputHandler()(event, nil)
+			}
+		} else if key == tcell.KeyCtrlS {
+			g.Save()
+		} else {
+			switch key {
+			case tcell.KeyDown, tcell.KeyUp:
+				g.grep_list_view.List.Focus(nil)
+			case tcell.KeyEnter:
+				g.grep_list_view.List.InputHandler()(event, nil)
+			}
+			// g.update_preview()
+			// g.update_title()
+		}
+	}
+}
+
+func RunQuery(g *greppicker) {
+	g.impl.fzf_on_result = nil
+	g.parent.input.SetLabel(">")
+	g.impl.query_option.query = g.parent.input.GetText()
+	g.livewgreppicker.__updatequery(g.impl.query_option)
+}
+
+// name implements picker.
+func (g *greppicker) name() string {
+	return "grep word"
+}
