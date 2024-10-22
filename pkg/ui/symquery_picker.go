8c10c4cbc0c3ecacf68bcaa32d7727ecb6278a49
wellcomez <zhu.jialai@gmail.com>
2024-10-22 14:46:18 +0800

    feat(ui): improve symbol picker functionality and display

--- pkg/ui/symquery_picker.go
+++ pkg/ui/symquery_picker.go
@@ -5,6 +5,7 @@ package mainui
 
 import (
 	"fmt"
+	"path/filepath"
 	"strings"
 
 	"github.com/gdamore/tcell/v2"
@@ -18,7 +19,7 @@ type workspace_query_picker_impl struct {
 	symbol *lspcore.Symbol_file
 	list   *customlist
 	query  string
-	sym    []lsp.SymbolInformation
+	// sym    []lsp.SymbolInformation
 }
 type workspace_query_picker struct {
 	impl *workspace_query_picker_impl
@@ -35,8 +36,6 @@ func (pk *workspace_query_picker) name() string {
 }
 
 func (pk *workspace_query_picker) on_query_ok(sym []lsp.SymbolInformation) {
-	pk.impl.sym = sym
-	root := global_prj_root
 	pk.impl.parent.app.QueueUpdateDraw(func() {
 		pk.impl.list.Key = pk.impl.query
 		for i, v := range sym {
@@ -55,16 +54,23 @@ func (pk *workspace_query_picker) on_query_ok(sym []lsp.SymbolInformation) {
 			colors := []colortext{
 				{fmt.Sprintf("%-10s", strings.ReplaceAll(v.Kind.String(), "SymbolKind:", "")), 0},
 				{fmt.Sprintf("%-20s ", strings.TrimLeft(v.Name, " \t")), fg},
-				{trim_project_filename(filename, root), 0},
+				{filepath.Base(filename), 0},
 			}
 			pk.impl.list.AddColorItem(colors, nil, func() {
-				sym := pk.impl.sym[index]
+				sym := sym[index]
 				main := pk.impl.parent.main
 				main.OpenFileHistory(sym.Location.URI.AsPath().String(), &sym.Location)
 				pk.impl.parent.hide()
 			})
 		}
-		pk.update_preview()
+		pk.impl.list.SetChangedFunc(func(index int, mainText, secondaryText string, shortcut rune) {
+			pk.update_preview(sym)
+			if index < len(sym) {
+				v := sym[index]
+				filename := v.Location.URI.AsPath().String()
+				pk.impl.list.SetTitle(trim_project_filename(filename, global_prj_root))
+			}
+		})
 	})
 }
 
@@ -82,10 +88,10 @@ func (pk *workspace_query_picker) UpdateQuery(query string) {
 		}
 	}()
 }
-func (pk workspace_query_picker) update_preview() {
+func (pk workspace_query_picker) update_preview(sym []lsp.SymbolInformation) {
 	cur := pk.impl.list.GetCurrentItem()
-	if cur < len(pk.impl.sym) {
-		item := pk.impl.sym[cur]
+	if cur < len(sym) {
+		item := sym[cur]
 		pk.impl.PrevOpen(item.Location.URI.AsPath().String(),
 			item.Location.Range.Start.Line)
 	}
@@ -93,7 +99,6 @@ func (pk workspace_query_picker) update_preview() {
 func (pk workspace_query_picker) handle_key_override(event *tcell.EventKey, setFocus func(p tview.Primitive)) {
 	handle := pk.impl.list.InputHandler()
 	handle(event, setFocus)
-	pk.update_preview()
 }
 
 // handle implements picker.
@@ -110,6 +115,7 @@ func new_workspace_symbol_picker(v *fzfmain, code CodeEditor) *workspace_query_p
 		},
 	}
 	ret.impl.prev_picker_impl.use_cusutom_list(ret.impl.list)
+
 	return ret
 }
 func (pk *workspace_query_picker) grid() *tview.Flex {
