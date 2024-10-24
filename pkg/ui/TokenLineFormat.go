8548b949ba02b05b9e20aeac363a09c3aae53d85
wellcomez <zhu.jialai@gmail.com>
2024-10-24 13:42:04 +0800

    refactor/pkg/ui: - fix Update SubTo and SubFrom methods for more accurate token handling

--- pkg/ui/TokenLineFormat.go
+++ pkg/ui/TokenLineFormat.go
@@ -23,9 +23,20 @@ func (format *TokenLineFormat) Run() {
 	for _, tl := range lines {
 		tl.Run(format)
 	}
+	debug.DebugLog("format", strings.Repeat("=", 20), len(lines))
 	for _, tl := range lines {
 		tl.print()
 	}
+	debug.DebugLog("format", strings.Repeat("=", 20), len(lines))
+	for _, tl := range lines {
+		i := tl.lineno
+		if tl.removed {
+			debug.DebugLog("format", "x:", i)
+		} else {
+			debug.DebugLog("format", "*:", i, tl.FormatOutput(true))
+		}
+	}
+	// debug.DebugLog("format", "%s\nformat", strings.Join(code, "\n"))
 
 }
 
@@ -41,7 +52,7 @@ func (currentLine TokenLine) print() {
 	}
 	debug.DebugLogf("format", "line:%d inline-editor:%d  line-editor:%s", currentLine.lineno, currentLine.editorcount, e)
 	debug.DebugLog("format", "old--", codeprint(currentLine.line))
-	debug.DebugLog("format", "new--", codeprint(currentLine.FormatOutput()))
+	debug.DebugLog("format", "new--", codeprint(currentLine.FormatOutput(false)))
 }
 func (currentLine *TokenLine) Run(format *TokenLineFormat) {
 	if currentLine.formated {
@@ -88,10 +99,9 @@ func (lastline *TokenLine) split(end femto.Loc) (replace []Token, add []Token) {
 func create_line(Buf *femto.Buffer, edits []lsp.TextEdit) (tokenline *TokenLine, next_index int) {
 	next_index = -1
 	if len(edits) > 0 {
-		tokenline = &TokenLine{}
 		lineNr := edits[0].Range.Start.Line
-		tokenline.lineno = lineNr
-		line := Buf.Line(lineNr)
+		tokenline = create_from_bufline(Buf, lineNr)
+		line := tokenline.line 
 
 		begin_x := 0
 		for next_index = 0; next_index < len(edits); next_index++ {
@@ -130,7 +140,6 @@ func create_line(Buf *femto.Buffer, edits []lsp.TextEdit) (tokenline *TokenLine,
 			}
 		}
 		tokenline.Tokens = s
-		tokenline.line = line
 
 	}
 	return
@@ -172,7 +181,7 @@ type TokenLine struct {
 
 func (line *TokenLine) SubTo(index int) (t []Token) {
 	for _, v := range line.Tokens {
-		if index < v.e {
+		if index <= v.e {
 			if index > v.b {
 				vv := v
 				vv.data = line.line[v.b:index]
@@ -188,20 +197,22 @@ func (line *TokenLine) SubTo(index int) (t []Token) {
 }
 func (line *TokenLine) SubFrom(index int) (t []Token) {
 	for _, v := range line.Tokens {
-		if index >= v.b {
+		if index <= v.b {
+			t = append(t, v)
+		} else {
 			if index < v.e {
 				t = append(t, Token{line.line[index:v.e], v.edit, index, v.e})
-			} else {
-				t = append(t, v)
 			}
 		}
 	}
 	t = append(t, line.appends...)
 	return
 }
-func (t *TokenLine) FormatOutput() (ret string) {
+func (t *TokenLine) FormatOutput(delet_empty bool) (ret string) {
 	if t.removed {
-		ret = "deleted?????????????????????????????????"
+		if !delet_empty {
+			ret = "deleted?????????????????????????????????"
+		}
 		return
 	}
 	if len(t.newline) > 0 {
@@ -234,13 +245,19 @@ func NewTokenLineFormat(Buf *femto.Buffer, edits []lsp.TextEdit) (f *TokenLineFo
 			if next_edit := line.line_edit; next_edit != nil {
 				start, stop := GetEditLoc(*next_edit)
 				for i := start.Y + 1; i < stop.Y+1; i++ {
-					x := Buf.Line(i)
-					token := Token{x, nil, 0, len(x)}
-					f.lines[i] = &TokenLine{[]Token{token}, 0, x, nil, i, nil, nil, nil, false, false}
+					x1 := create_from_bufline(Buf, i)
+					f.lines[i] = x1
 				}
 			}
 		} else {
 			return
 		}
 	}
+}
+
+func create_from_bufline(Buf *femto.Buffer, i int) *TokenLine {
+	x := Buf.Line(i)
+	token := Token{x, nil, 0, len(x)}
+	x1 := TokenLine{[]Token{token}, 0, x, nil, i, nil, nil, nil, false, false}
+	return &x1
 }
