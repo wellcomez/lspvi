9f3e72ea999796214ec620cc4826c97ca5ac27b4
wellcomez <zhu.jialai@gmail.com>
2024-10-31 21:58:52 +0800

    fix(treesitter): treesitter remove define for javascript outline

--- pkg/lsp/queries/javascript/outline.scm
+++ pkg/lsp/queries/javascript/outline.scm
@@ -1,11 +1,3 @@
-(internal_module
-    "namespace" @context
-    name: (_) @name) @item
-
-(enum_declaration
-    "enum" @context
-    name: (_) @name) @item
-
 (function_declaration
     "async"? @context
     "function" @context
@@ -14,9 +6,6 @@
       "(" @context
       ")" @context)) @item
 
-(interface_declaration
-    "interface" @context
-    name: (_) @name) @item
 
 (program
     (export_statement
@@ -41,25 +30,13 @@
         "set"
         "async"
         "*"
-        "readonly"
         "static"
-        (override_modifier)
-        (accessibility_modifier)
     ]* @context
     name: (_) @name
     parameters: (formal_parameters
       "(" @context
       ")" @context)) @item
 
-(public_field_definition
-    [
-        "declare"
-        "readonly"
-        "abstract"
-        "static"
-        (accessibility_modifier)
-    ]* @context
-    name: (_) @name) @item
 
 ; Add support for (node:test, bun:test and Jest) runnable
 (call_expression
