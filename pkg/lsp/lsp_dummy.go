8b67e961675f6da0763a3040244302e107318bbf
wellcomez <zhu.jialai@gmail.com>
2024-11-05 07:57:48 +0800

    refactor(lsp): split lsp_treesitter.go into separate files
    - Move ts_lang_def struct and related functions to treesitter_lang_def.go
    - Create new file lsp_dummy.go with lsp_dummy struct and methods
    - Remove redundant code and imports from lsp_treesitter.go

---
+++ pkg/lsp/lsp_dummy.go
@@ -0,0 +1,31 @@
+package lspcore
+
+import "github.com/tectiv3/go-lsp"
+type lsp_dummy struct {
+	lsplang_base
+}
+
+// InitializeLsp implements lsplang.
+func (l lsp_dummy) InitializeLsp(core *lspcore, wk WorkSpace) error {
+	return nil
+}
+
+// IsMe implements lsplang.
+func (l lsp_dummy) IsMe(filename string) bool {
+	return false
+}
+
+// IsSource implements lsplang.
+func (l lsp_dummy) IsSource(filename string) bool {
+	return false
+}
+
+// Launch_Lsp_Server implements lsplang.
+func (l lsp_dummy) Launch_Lsp_Server(core *lspcore, wk WorkSpace) error {
+	return nil
+}
+
+// Resolve implements lsplang.
+func (l lsp_dummy) Resolve(sym lsp.SymbolInformation, symfile *Symbol_file) bool {
+	return false
+}
