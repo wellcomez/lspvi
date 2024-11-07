ff48b8215519a086a9f35c39c15f15d7255af7d7
wellcomez <zhu.jialai@gmail.com>
2024-11-07 14:19:52 +0800

    refactor(ui): move hover test functionality to diagnostic package
    - Extract hover test logic from codeview.go to diagnostic.go
    - Improve code organization and separation of concerns
    - Update import statements and package dependencies

--- pkg/ui/diagnostic.go
+++ pkg/ui/diagnostic.go
@@ -1,6 +1,14 @@
 package mainui
 
-import "github.com/tectiv3/go-lsp"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/pgavlin/femto"
+	"github.com/rivo/tview"
+	"github.com/tectiv3/go-lsp"
+)
 
 type Diagnostic interface {
 }
@@ -44,5 +52,52 @@ func (prj *project_diagnostic) Update(diags lsp.PublishDiagnosticsParams) {
 	}
 	if !diags.IsClear {
 		prj.data = append(prj.data, *NewLspDiagnostic(diags))
+	}
+}
+func hove_test(root *codetextview, pos mouse_event_pos, event *tcell.EventMouse) {
+	if dia := root.code.Dianostic(); !dia.data.IsClear {
+		var new_diagnos_hove = func(v lsp.Diagnostic, mouse tcell.EventMouse) {
+			root.error = nil
+			buff_loc := femto.Loc{
+				X: pos.X,
+				Y: pos.Y,
+			}
+			root.hover = new_hover(buff_loc, func() {
+				msg := &LspTextView{
+					Box:  tview.NewBox(),
+					main: root.main,
+				}
+				ss := []string{" " + v.Message + " ", fmt.Sprintf(" %s %d:%d", root.filename, buff_loc.X, buff_loc.Y+1), v.Source}
+				msg.Load(strings.Join(ss, "\n"), root.code.Path())
+
+				go func() {
+					root.main.App().QueueUpdate(func() {
+						w := 0
+						h := len(msg.lines)
+						for _, v := range msg.lines {
+							w = max(w, len(v))
+						}
+						ss := &mouse
+						_, y := ss.Position()
+						edit_x, _, _, _ := root.GetInnerRect()
+						x := v.Range.Start.Character + edit_x
+						msg.SetRect(x, y+1, w, h)
+						root.error = msg
+					})
+				}()
+			})
+		}
+		for i := range dia.data.Diagnostics {
+			v := dia.data.Diagnostics[i]
+			var ok = v.Severity == lsp.DiagnosticSeverityError || v.Severity == lsp.DiagnosticSeverityWarning
+			if ok && v.Range.Start.Line == pos.Y {
+				if hover := root.hover; hover == nil {
+					new_diagnos_hove(v, *event)
+				} else if hover.Pos.Y != pos.Y {
+					new_diagnos_hove(v, *event)
+				}
+				break
+			}
+		}
 	}
 }
