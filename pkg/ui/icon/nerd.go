f2de014309ac0b4d47ed33b334f07817b48f2ffa
wellcomez <zhu.jialai@gmail.com>
2024-11-12 21:33:27 +0800

    fix(femto): update submodule to latest commit
    - Update femto submodule from d431e28 to 8413a02

---
+++ pkg/ui/icon/nerd.go
@@ -0,0 +1,28 @@
+package nerd
+
+var Nf_cod_file_binary = ''   //=\ueae8
+var Nf_cod_call_incoming = '' // =\ueb92
+var Nf_md_call_received = '\U000f00fa'
+var Nf_cod_call_outgoing = '' // =\ueb93
+var Nf_oct_search = ''        //=\uf422
+var Nf_cod_folder = ''        // =\uea83
+var Nf_fa_bookmark = ''
+var Nf_md_alpha_i_box = '󰬐'       // =\uf0b10
+var Nf_fa_window_close = ''      // =\uf2d3
+var Nf_cod_split_horizontal = '' // =\ueb56
+var Nf_md_dock_bottom = '󱂩'       // =\uf10a9
+var Nf_cod_zoom_out = ''         // =\ueb82
+var Nf_cod_zoom_in = ''          // =\ueb81
+var Nf_cod_symbol_misc = ''      // =\ueb63
+var Nf_fa_save = ''              // =\uf0c7
+var Nf_cod_exclude = ''          // =\ueae5
+var Nf_cod_case_sensitive = ''   // =\ueab1
+var Nf_cod_whole_word = ''       //=\ueb7e
+// var Nf_md_arrow_down_drop_circle = '\U000f004a'
+var Nf_md_arrow_down_drop_circle = '󰁊' // =\uf004a
+var Nf_md_dock_left = '󱂪'              // =\uf10aa
+var Nf_md_dock_right = '󱂫'             //=\uf10ab
+var Nf_md_chevron_right_circle = '󰬪'   // =\uf0b2a
+var Nf_md_chevron_left_circle = '󰬨'    // =\uf0b28
+// var 7207:   {"nf-fa-google_plus_square", '\uf0d4'},
+var Nf_fa_square_google_plus = '' // =\uf0d4
