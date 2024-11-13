4aa4556a6929587c2636d18e75311eaf08e8e59b
wellcomez <zhu.jialai@gmail.com>
2024-11-13 23:23:07 +0800

    style(ui): add monokai theme for highlight.js
    - Ported the Monokai style for highlight.js by Luigi Maselli
    - Added monokai.css file to the highlightjs styles directory
    - Defined color schemes and font styles for various code elements

--- pkg/ui/xterm/markdowit/build.sh
+++ pkg/ui/xterm/markdowit/build.sh
@@ -2,4 +2,4 @@
 # cp dist/mark.js md.html ../html/
 npm run build
 cp dist/mark.umd.js ../html/mark.js
-cp md.html ../html/
+# cp md.html ../html/
--- pkg/ui/xterm/markdowit/main.js
+++ pkg/ui/xterm/markdowit/main.js
@@ -1,6 +1,11 @@
 import markdownit from 'markdown-it'
 import hljs from 'highlight.js' // https://highlightjs.org
 import { plantuml } from "@mdit/plugin-plantuml";
+import anchor from "markdown-it-anchor"
+import toc from "markdown-it-table-of-contents"
+// import 'highlight.js/styles/github-dark.min.css';
+import HighlightJS from 'markdown-it-highlightjs'
+
 
 // Actual default values
 const md = markdownit({
@@ -17,6 +22,13 @@ const md = markdownit({
   }
 });
 md.use(plantuml);
+
+md.use(anchor.default); // Optional, but makes sense as you really want to link to something, see info about recommended plugins below
+md.use(toc);
+const opts = {
+  hljs: hljs
+}
+md.use(HighlightJS, opts)
 function render(text) {
   return md.render(text);
 }
