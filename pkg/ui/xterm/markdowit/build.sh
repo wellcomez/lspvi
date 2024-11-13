a49618c9b40da8246b1527cb2a65110135c1cf70
wellcomez <zhu.jialai@gmail.com>
2024-11-14 01:13:12 +0800

    add sourcemap

--- pkg/ui/xterm/markdowit/build.sh
+++ pkg/ui/xterm/markdowit/build.sh
@@ -1,5 +1,7 @@
 # npm run rel
 # cp dist/mark.js md.html ../html/
-npm run build
+# npm run build
+npm run dev
 cp dist/mark.umd.js ../html/mark.js
+cp dist/mark.umd.js.map ../html/
 # cp md.html ../html/
