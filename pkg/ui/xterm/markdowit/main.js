450878a9f872b0830eb5ba2bf358d66bb80446ed
wellcomez <zhu.jialai@gmail.com>
2024-11-13 23:55:33 +0800

    update code padding

--- pkg/ui/xterm/markdowit/main.js
+++ pkg/ui/xterm/markdowit/main.js
@@ -5,18 +5,32 @@ import anchor from "markdown-it-anchor"
 import toc from "markdown-it-table-of-contents"
 // import 'highlight.js/styles/github-dark.min.css';
 import HighlightJS from 'markdown-it-highlightjs'
+// import 'highlight.js/styles/monokai-sublime.css'; // 引入你喜欢的主题样式文件
 
 
 // Actual default values
-const md = markdownit({});
+const md = markdownit({
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs" style="padding:10px"><code class="language-' + lang + '">' +
+          hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
+          '</code></pre>';
+      } catch (__) { }
+    }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
+})
+
 md.use(plantuml);
 
 md.use(anchor.default); // Optional, but makes sense as you really want to link to something, see info about recommended plugins below
 md.use(toc);
-const opts = {
-  hljs: hljs
-}
-md.use(HighlightJS, opts)
+// const opts = {
+// hljs: hljs
+// }
+// md.use(HighlightJS, opts)
 function render(text) {
   return md.render(text);
 }
