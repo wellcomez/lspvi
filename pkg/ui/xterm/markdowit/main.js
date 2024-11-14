5f507d52c270cb3cbe00fadc7f206abf0a2b2482
wellcomez <zhu.jialai@gmail.com>
2024-11-14 10:38:46 +0800

    refactor

--- pkg/ui/xterm/markdowit/main.js
+++ pkg/ui/xterm/markdowit/main.js
@@ -2,8 +2,8 @@ import markdownit from 'markdown-it'
 import hljs from 'highlight.js' // https://highlightjs.org
 import { plantuml } from "@mdit/plugin-plantuml";
 import axios from 'axios'
-// import anchor from "markdown-it-anchor"
-// import toc from "markdown-it-table-of-contents"
+import anchor from "markdown-it-anchor"
+import toc from "markdown-it-table-of-contents"
 // import 'highlight.js/styles/github-dark.min.css';
 // import HighlightJS from 'markdown-it-highlightjs'
 // import 'highlight.js/styles/monokai-sublime.css'; // 引入你喜欢的主题样式文件
@@ -47,8 +47,8 @@ const md = markdownit({
 // };
 md.use(plantuml);
 
-// md.use(anchor.default); // Optional, but makes sense as you really want to link to something, see info about recommended plugins below
-// md.use(toc);
+md.use(anchor.default); // Optional, but makes sense as you really want to link to something, see info about recommended plugins below
+md.use(toc);
 // const opts = {
 // hljs: hljs
 // }
